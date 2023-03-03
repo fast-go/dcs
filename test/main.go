c2d461c33fbf9262afa32ec0f726603d6c3b7f14
13235024110 <congrong512@vip.qq.com>
2023-03-03 16:38:12 +0800

    更新测试

--- test/main.go
+++ test/main.go
@@ -1,19 +1,51 @@
 package main
 
-import "github.com/zeromicro/go-zero/core/logx"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	logx.MustSetup(logx.LogConf{
-		Mode: "file",
-		Path: "./logs",
-	})
-	logx.Error("测试")
+	b := make(chan int, 2)
 
-	//for i := 9; i < 100; i++ {
-	//	r, _ := http.Post("http://175.178.49.188:30389/base/login", "", bytes.NewBufferString(""))
-	//
-	//	b, _ := ioutil.ReadAll(r.Body)
-	//	fmt.Println(string(b))
-	//}
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for i := 0; i <= 10; i++ {
+			select {
+			case <-ctx.Done():
+				fmt.Println("获取到关闭信息 <-ctx.Done()")
+				return
+
+			case b <- i:
+				fmt.Println(fmt.Sprintf("输入进去%d", i))
+				time.Sleep(time.Second * 1)
+			}
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("获取信息也能拿到关闭状态 <-ctx.Done()")
+				return
+
+			case res, ok := <-b:
+				fmt.Println(fmt.Sprintf("获取到信息%d:%+v", res, ok))
+				time.Sleep(time.Second * 1)
+
+			}
+
+		}
+	}()
+
+	time.Sleep(time.Second * 3)
+
+	//c <- 1
+	cancel()
+	close(b)
+	time.Sleep(time.Second * 1000)
+
 }
